f2262c3821e02a7ed62eaa63b5e3cf7b26111a36
AJ-505 <abasiono.mbat@gmail.com>
2025-11-29 15:25:37 +0100

    Feat: Week 10 Assignment

---
+++ week-10/array-manipulation-assignment.cpp
@@ -0,0 +1,115 @@
+#include <cstdlib>
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int findSizeOfArray(int arr[]) {
+  return sizeof(arr) / sizeof(arr[0]);
+}
+
+int generateRandomNumbersBetweenRange(int lowerBound, int upperBound) {
+  // Generates a random number between two boundaries (both exclusive hence in the form [lowerBound..=upperBound])
+  return (rand() % (upperBound + 1)) + lowerBound;
+}
+
+void traverseAndPrintArrayElements(int arr[]) {
+  int size = findSizeOfArray(arr);
+
+  for (int i = 0; i < size; ++i) {
+    // Prints elements in the form 1, 2, 3, 4, and 5. (for example)
+
+    if (i == size - 1) {
+      cout << "and " << arr[i] << ".";
+    } else {
+      cout << arr[i] << ", ";
+    }
+  }
+}
+
+int findMaxElement(int arr[]) {
+  int max = arr[0];
+  int size = findSizeOfArray(arr);
+  
+  for (int i = 1; i < size; ++i) {
+    if (arr[i] > max) max = arr[i];
+  }
+
+  return max;
+}
+
+int findMinElement(int arr[]) {
+  int min = arr[0];
+  int size = findSizeOfArray(arr);
+  
+  for (int i = 1; i < size; ++i) {
+    if (arr[i] < min) min = arr[i];
+  }
+
+  return min;
+}
+
+int sumAllElements(int arr[]) {
+  int size = findSizeOfArray(arr);
+  int sum = 0;
+
+  for (int i = 1; i < size; ++i)  sum += arr[i]; 
+  return sum;
+}
+
+int returnIndexOfArrayElement(int arr[], int elementToFind) {
+  int size = findSizeOfArray(arr);
+
+  for (int i = 1; i < size; ++i) {
+    if (arr[i] == elementToFind) return i;
+  } 
+
+  return -1;
+}
+
+int main() {
+  srand(time(0));
+  cout << "Hello there! Welcome to my lovely program!\n";
+  // Step 1: Get the user-defined array
+  int arrSize;
+
+  cout << "Enter the size of your array (numeric values only): ";
+  cin >> arrSize;
+  int customArray[arrSize];
+
+  for (int i = 0; i < arrSize; ++i) {
+    cout << "Enter array element at index " << i << ": ";
+    cin >> customArray[i];
+  }
+
+  cout << "The minimum number in the array is: " << findMinElement(customArray) << endl;
+  cout << "The maximum number in the array is: " << findMaxElement(customArray) << endl;
+  cout << "The sum of all numbers (elements) in the array is: " << sumAllElements(customArray) << endl;
+  
+  cout << "Aren't you impressed? One more thing... \n ";
+  cout << "Do you remember all the elements in this array? Surely not! But thankfully, you can just check! \n";
+  cout << "Please enter a number to see whether it exists in the array: ";
+
+  int userNum; cin >> userNum;
+
+  // Conditional logic to check whether the element in question exists in the array
+
+  if (returnIndexOfArrayElement(customArray, userNum) != -1) {
+    cout << "The element " << userNum << "exists in the array! Woohoo!\n";
+  } else {
+    cout << "The element " << userNum << "does not exist in the array.\n";
+  }
+
+  cout << "Ready for one more trick?\n";
+  cout << "We can generate random numbers and save them into an array. Isn't that cool? Let's see it in action!\nRun this program multiple times to see the beauty of randomness.\n";
+
+  int randomSize = generateRandomNumbersBetweenRange(0, 20); //A number between 0 and 20
+  int randomArray[randomSize];
+  
+  for (int j = 0; j < randomSize; ++j) {
+    randomArray[j] = generateRandomNumbersBetweenRange(0, 20); 
+  }
+
+  cout << "The array elements are: "; traverseAndPrintArrayElements(randomArray);
+  return 0;
+}
